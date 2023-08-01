37d8be9112aca29984814aff3c031a012ac294f0
Elena Schall <eschall@vpn-172-26-1-178.awi.de>
2023-08-01 17:28:30 +0200

    Implemented that one can predict with saved model on the test files loaded from the csv

--- predict_with_old_model.py
+++ predict_with_old_model.py
@@ -1,61 +1,126 @@
 import pathlib
 import json
+import cv2
+import os
 
 import pandas as pd
 import tensorflow as tf
-from sklearn.metrics import RocCurveDisplay, roc_curve
-import matplotlib.pyplot as plt
 import numpy as np
 
 import dataset
+import training
+import metrics
+
+data_folder = pathlib.Path('/Users/eschall/Documents/Python/CNN+Noise/Specs')
+model_to_analyze_folder = pathlib.Path('/Users/eschall/Documents/Python/CNN+Noise/Results/230731_152252/foldBallenyIsland2015/model')
+config_folder = pathlib.Path('/Users/eschall/Documents/Python/CNN+Noise/Results/230731_152252')
+csv_split_file = pathlib.Path('/Users/eschall/Documents/Python/CNN+Noise/Results/230731_152252/data_used_foldBallenyIsland2015_noiseall.csv')
+
+IMAGE_HEIGHT = 90
+IMAGE_WIDTH = 30
+N_CHANNELS = 1
+
+class SpectrogramDataSet:
+    def __init__(self,  image_width, image_height, categories, join_cat, n_channels):
+        """
+
+        :param image_width:
+        :param image_height:
+        :param categories:
+        :param join_cat:
+
+        """
+
+        self.image_height = image_height
+        self.image_width = image_width
+        self.categories = categories
+        self.join_cat = join_cat
+        self.n_channels = n_channels
+
+        # Create an understandable map for joined categories
+        # and their corresponding int representation
+        self.map_join = {}
+        self.classes2int = {}
+        self.int2class = []
+        for join_class_name, classes_list in join_cat.items():
+            self.classes2int[join_class_name] = len(self.classes2int.keys())
+            self.int2class.append(join_class_name)
+            for class_name in classes_list:
+                self.map_join[class_name] = join_class_name
+
+        for cat_name in self.categories:
+            if cat_name not in self.map_join.keys():
+                self.map_join[cat_name] = cat_name
+                self.classes2int[cat_name] = len(self.classes2int)
+                self.int2class.append(cat_name)
+
+    def reshape_images(self, images):
+        """
+        Reshape all the images to the specified height, width and channels during init of object
+
+        :param images: list of images
+        :return: array with normalized images (0 to 1) with the correct shape
+        """
+        X = np.array(images).reshape(-1, self.image_width, self.image_height, self.n_channels)
+        x = X / 255.0
+        return x
+
+def predict_model_full_dataset(data_folder, model_folder, config_folder, csv_split_file):
+    #  Load the test dataset
+    x_test, y_test, ds = load_dataset_from_csv(csv_split_file=csv_split_file, data_folder=data_folder, config_folder=config_folder)
+
+    detection_metrics = metrics.ImbalancedDetectionMatrix(noise_class_name='Noise', classes_names=ds.int2class)
+    cnn_model = tf.keras.models.load_model(model_folder, custom_objects={"imbalanced_metric": detection_metrics.imbalanced_metric, "noise_misclas_rate": detection_metrics.noise_misclas_rate, "call_avg_tpr": detection_metrics.call_avg_tpr, "f1_score": metrics.f1_score, "recall_score": metrics.recall_score, "accuracy":tf.keras.metrics.SparseCategoricalAccuracy(name='accuracy')},)
+
+    scores_i, con_mat_df = training.get_model_scores(cnn_model, x_test=x_test, y_test=y_test, categories=ds.int2class)
 
 
-def predict_model_full_dataset(data_folder, model_folder):
-    config_file = model_folder.joinpath('config.json')
+    return scores_i, con_mat_df
+
+
+def load_dataset_from_csv(csv_split_file, data_folder, config_folder):
+
+    config_file = config_folder.joinpath('config.json')
 
     # Read the config file
     f = open(config_file)
     config = json.load(f)
 
-    rois_path = data_folder.joinpath(config['data_file'])
-    rois_df = pd.read_csv(rois_path)
+    ds = SpectrogramDataSet(image_width=IMAGE_WIDTH, image_height=IMAGE_HEIGHT,
+                        categories=config['CATEGORIES'], join_cat=config["CATEGORIES_TO_JOIN"],
+                        n_channels=N_CHANNELS)
 
-    #  Create the dataset
-    ds = dataset.AudioDataSetROI(data_folder, rois_df, fs=config['SAMPLING_RATE'], block_time=config['BLOCK_TIME'],
-                                 pad_time=config['PAD_TIME'])
+    data_split_df = pd.read_csv(csv_split_file)
+    data_split_test = data_split_df[data_split_df['set'] == 'test']
+    images_for_test = pd.Series(data_split_test['path'])
 
-    model = tf.keras.models.load_model(model_folder.joinpath('model'))
+    labels = []
+    images = []
+    for img_path in images_for_test:
+        cat_folder = os.path.splitext(os.path.basename(img_path))[0].split('_')[2]
+        img_array = cv2.imread(os.path.join(data_folder, cat_folder, img_path))
 
-    ds.predict_with_model(model, output_folder=model_folder,
-                          threshold=config['threshold_results'])
+        # Not necessary if images already on the correct format
+        # resized_image = cv2.resize(img_array, (self.image_width, self.image_height))
+        grey_image = np.mean(img_array, axis=2)
+        images.append(grey_image)
 
+        # This part is for joined classes
+        labels.append(ds.classes2int[ds.map_join[cat_folder]])
 
-def plot_roc_curve(csv_path_with_prob):
-    prob_csv = pd.read_csv(csv_path_with_prob)
-    prob_csv = prob_csv.dropna()
-    y_test = prob_csv.Label != 'Noise'
-    y_pred = prob_csv['prob']
+    x = ds.reshape_images(images)
+    y = np.array(labels)
 
-    fpr, tpr, thresholds_list = roc_curve(y_test, y_pred)
-    fpr_1percent = np.argmin(np.abs(fpr - 0.01))
-    tpr_1percernt = tpr[fpr_1percent]
-    threshold = thresholds_list[fpr_1percent]
-    print('TPR for 1% FPR: ', tpr_1percernt)
-    print('Treshold where that happens: ', threshold)
-    RocCurveDisplay.from_predictions(y_test, y_pred)
-    plt.savefig(csv_path_with_prob.parent.joinpath('ROC_curve.png'))
-    plt.show()
-
-    roc_df = pd.DataFrame(data={'fpr': fpr, 'tpr': tpr, 'threshold': thresholds_list})
-    roc_df.to_csv(csv_path_with_prob.parent.joinpath('ROC_values.csv'))
+    return x, y, ds
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    data_folder = pathlib.Path('./data')
-    models_folder = pathlib.Path('./transfer_models')
 
-    model_to_analyze_folder = models_folder.joinpath('transfer_mammals', '220727_170706')
     # Test an old model
-    predict_model_full_dataset(data_folder=data_folder, model_folder=model_to_analyze_folder)
-    plot_roc_curve(model_to_analyze_folder.joinpath('probability_predictions.csv'))
+    scores, con_matrix = predict_model_full_dataset(data_folder=data_folder, model_folder=model_to_analyze_folder, config_folder=config_folder, csv_split_file=csv_split_file)
+
+    log_path = pathlib.Path(os.path.split(model_to_analyze_folder)[0])
+    con_matrix.to_csv(log_path.joinpath('confusion_matrix.csv'))
+    scores.to_csv(log_path.joinpath('scores.csv'))
+
